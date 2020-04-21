d004bed60376f39c5fb88e0d5489766172e4227b
qiangyongjun918 <qiangyongjun918@163.com>
2020-04-21 15:05:08 +0800

    引入字体图标

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,32 +1,27 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">55555</text>
-		</view>
-	</view>
+  <view class="content">
+    <text class="iconfont iconvideocamera">55555</text>
+  </view>
 </template>
 
 <script>
-	export default {
-
-	}
+export default {};
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
+.content {
+  text-align: center;
+  height: 400upx;
+}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
+.logo {
+  height: 200upx;
+  width: 200upx;
+  margin-top: 200upx;
+}
 
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+.title {
+  font-size: 36upx;
+  color: #8f8f94;
+}
 </style>
